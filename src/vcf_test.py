2706757f79bd38e79a31e361d12ec79c0a48a511
andrewtsun25 <andrewtsun25@gmail.com>
2016-05-22 20:48:25 -0700

    vcf demo syntactically correct. change paths as necesssary.

--- src/vcf_test.py
+++ src/vcf_test.py
@@ -2,5 +2,5 @@ import vcf
 
 vcf_path = '../resources/chrom1.vcf'
 test_vcf = vcf.Reader(vcf_path, 'w')
-for i, record in enumerate(vcf_path)
+for i, record in enumerate(vcf_path):
     print('Record #{}: {}'.format(i, record))
