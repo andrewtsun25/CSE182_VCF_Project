9f6adb1de4f86d2354c979b6a60d2422166d0e50
dannie1993 <dannie1993@gmail.com>
2016-05-26 20:45:42 -0700

    rsid to omim id

--- src/rsid_to_omim.py
+++ src/rsid_to_omim.py
@@ -13,19 +13,13 @@ omim_id_regexp = re.compile('[0-9]+')
 for i in line:
     linesplit = i.split('|')
     if linesplit[0][:2] == 'rs':
-        #print (i)
-        #rsid_omim["rsid"] = linesplit[0]
         rsid = linesplit[0][:-1]
 
     if len(linesplit) > 2 and linesplit[1] == ' OMIM-CURATED-RECORDS ':
-        #print (i)
-        #rsid_omim["omim"] = linesplit[0]
         omim_id = linesplit[2][1:-1]
         is_omim_id = omim_id_regexp.match(omim_id)
         if is_omim_id:
             omim_temp.append(omim_id)
-
-    #print (rsid_omim)
 
     if linesplit[0] == '\n' and omim_temp:
         rsid_omim[rsid] = omim_temp
