fe8408578fb415eba4e721595b94cbdd47d5fe59
dannie1993 <dannie1993@gmail.com>
2016-05-31 09:22:46 -0700

    Formed from OmimVarLocusIdSNP.bcp

--- src/rsid_to_omim.py
+++ src/rsid_to_omim.py
@@ -1,32 +1,37 @@
 from collections import defaultdict
 import re
 
-with open('snp_result.txt') as linetext:
+with open('OmimVarLocusIdSNP.bcp') as linetext:
     line = linetext.readlines()
 
-rsid_to_omim_dicts = []
-rsid_omim = {}
-omim_temp = []
-rsid_temp = ""
-omim_id_regexp = re.compile('[0-9]+')
+def rsid_to_omim(line):
+    rsid_to_omim_dicts = []
+    rsid_omim = {}
+    omim_temp = []
+    #rsid_temp = ""
+    omim_id_regexp = re.compile('[0-9]+')
 
-for i in line:
-    linesplit = i.split('|')
-    if linesplit[0][:2] == 'rs':
-        rsid = linesplit[0][:-1]
+    for i in line:
+        linesplit = i.split('\t')
+        rsid = 'rs' + linesplit[len(linesplit)-1][:-1]
 
-    if len(linesplit) > 2 and linesplit[1] == ' OMIM-CURATED-RECORDS ':
-        omim_id = linesplit[2][1:-1]
+        omim_id = linesplit[0]
         is_omim_id = omim_id_regexp.match(omim_id)
+
         if is_omim_id:
             omim_temp.append(omim_id)
 
-    if linesplit[0] == '\n' and omim_temp:
         rsid_omim[rsid] = omim_temp
         rsid_to_omim_dicts.append(rsid_omim)
         rsid_omim = {}
         omim_temp = []
-        rsid_temp = ""
+        #rsid_temp = ""
+    return rsid_to_omim_dicts
 
-for i in rsid_to_omim_dicts:
+convert = rsid_to_omim(line)
+
+for i in convert:
     print (i)
+
+#for i in range (2000):
+#    print (line[i])
